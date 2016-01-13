f4c1f9c040a07a9a86254ad1657bc6180a497b11
Kosuke Kawahira <kawahira@binzume.net>
2016-01-14 02:23:00 +0900

    display gen_server initial_call.

--- webapp.rb
+++ webapp.rb
@@ -32,11 +32,12 @@ class WebApp < Sinatra::Base
   end
 
   get '/procs' do
-      node = "test01@hoge"
+      selfnode = "test01@hoge"
       cookie = File.read(ENV['HOME']+"/.erlang.cookie")
-      erl = Erlang::Erl.new(node, cookie)
+      erl = Erlang::Erl.new(selfnode, cookie)
 
       proc_infos = erl.nodes.reduce([]){|acc, node|
+        get_name_fun = erl.eval(node, "fun(P)-> {dictionary,D}=erlang:process_info(P, dictionary), lists:keyfind('$initial_call',1, D) end.")
         procs = erl.rpc_call(node, :erlang, :processes, [])
         acc += procs.map{|proc|
           info = erl.rpc_call(node, :erlang, :process_info, [proc, [:initial_call, :links, :monitors, :message_queue_len, :total_heap_size, :registered_name]])
@@ -45,6 +46,12 @@ class WebApp < Sinatra::Base
             monitors = infomap[:monitors].map{|m| m[1]}
             registered_name = infomap[:registered_name].empty? ? nil : infomap[:registered_name];
             initial_call = infomap[:initial_call][0].to_s + ":" + infomap[:initial_call][1].to_s
+            if infomap[:initial_call][0] == :proc_lib
+              d = erl.rpc_call(node, :erlang, :apply, [get_name_fun, [proc]])
+              if d != :false
+                initial_call = d[1][0].to_s + ":" + d[1][1].to_s
+              end
+            end
             infomap.merge({name: proc, alive: true, init_module: initial_call, registered_name: registered_name, monitors: monitors})
           else
             {name: proc, alive: false}

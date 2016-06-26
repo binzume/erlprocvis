d5f00b825e1156338a16022a477bc5a1416ac703
Kosuke Kawahira <kawahira@binzume.net>
2016-06-27 04:32:54 +0900

    display remote node.

--- public/js/procvis.js
+++ public/js/procvis.js
@@ -7,6 +7,9 @@ var procidx = {};
 var search = "";
 var defaultNodeColor = [0.6, 1.0, 1.0, 1.0];
 var nodeTexSize = 256;
+var displayPort = true;
+var displayRemote = true;
+var displayRemoteProc = false;
 
 function circleText(ctx, str, cx, cy, r, st) {
 	ctx.save();
@@ -27,6 +30,7 @@ function check_url_fragment() {
 		// init nodes
 		for (var i=0; i<nodes.length; i++) {
 			var node = nodes[i];
+			if (node.type != 'proc') continue;
 			if (node.proc.initial_call.startsWith(search) || node.proc.registered_name && node.proc.registered_name.startsWith(search)) {
 				node.color = [0, 1.0, 0, 1.0];
 			} else {
@@ -50,36 +54,70 @@ function proc_loaded(result) {
 	nodes = [];
 	edges = [];
 	procidx = {};
-	procs = result.processes;
+	var procs = result.processes;
 
 	for (var i=0; i<procs.length; i++) {
 		var proc = procs[i];
 		if (!proc.alive) continue;
 		procidx[proc.id] = nodes.length;
-		nodes.push({type: "proc", name: proc.id, proc: proc, weight: 1.0, links: []});
+		nodes.push({type: "proc", name: proc.id, proc: proc, weight: 1.0, weight2: Math.max(1,proc.links.length), links: []});
 	}
-	
+
 	for (var i=0; i<procs.length; i++) {
 		var proc = procs[i];
 		if (!proc.alive) continue;
+		var procnode = proc.id.split(':')[0];
 		for (var j=0; j<proc.links.length; j++) {
 			var l = proc.links[j];
-			if (proc.id < l) {
+			var addedge = proc.id < l;
+			var edgeweight = 1.0;
+			if (proc.links.length > 1) {
+				// node weight = node.links.length
+				// edgeweight = 1.0 / (proc.links.length-1);
+			}
+			var lineUV = 0;
+			if (!l.startsWith(procnode)) {
+				// remote or port
+				addedge = true;
+				if (l.startsWith('<#Port:')) {
+					edgeweight *= 0.5;
+					lineUV = 0.6;
+					if ( !procidx[l] && displayPort) {
+						procidx[l] = nodes.length;
+						nodes.push({type: "port", name: l, weight: 1.0, weight2: 1.0, links: [], color: [0,1.0,0,0.5]});
+					}
+				} else {
+					var nodename = l.split(':')[0].slice(1);
+					if (!displayRemoteProc) l = nodename;
+					edgeweight *= 0.001;
+					lineUV = 0.5;
+					if ( !procidx[l] && displayRemote) {
+						procidx[l] = nodes.length;
+						nodes.push({type: "node", name: l, weight: 1.0, weight2: 1.0, node: nodename, links: [], color: [1.0,1.0,0,0.5]});
+					}
+				}
+			}
+			if (addedge) {
 				// console.log("link: " + proc.id + " <-> " + l);
 				if (procidx[l]!=null && procidx[proc.id]!=null) {
-					edges.push({from: procidx[proc.id], to: procidx[l], weight: 1.0, type: "link"});
+					var ls = proc.links.length;
+					if (nodes[procidx[l]].proc) {
+						ls += nodes[procidx[l]].proc.links.length;
+					} else if (nodes[procidx[l]].type == 'node') {
+						ls += procs.length / 2;
+					}
+					edges.push({from: procidx[proc.id], to: procidx[l], weight: edgeweight, len: Math.sqrt(ls - 1), type: "link", line_uv: lineUV});
 					nodes[procidx[proc.id]].links.push(l);
 					nodes[procidx[l]].links.push(proc.id);
 				}
-				
 			}
 		}
-	
+
 		for (var j=0; j<proc.monitors.length; j++) {
 			var l = proc.monitors[j];
 			// console.log("monitor: " + proc.id + " -> " + l);
 			if (procidx[l]!=null && procidx[proc.id]!=null) {
-				edges.push({from: procidx[proc.id], to: procidx[l], weight: 1.0, type: "monitor"});
+				edges.push({from: procidx[proc.id], to: procidx[l], weight: 0, len: 1.0, type: "monitor", line_uv:0.5});
 			}
 		}
 	}
@@ -93,17 +131,16 @@ function proc_loaded(result) {
 	// init nodes
 	for (var i=0; i<nodes.length; i++) {
 		var node = nodes[i];
-		node.edges = [];
 
-		if (oldprocidx[node.proc.id] != null ) {
+		if (oldprocidx[node.name] != null ) {
 			// restore old position.
-			var oldnode = oldnodes[oldprocidx[node.proc.id]];
+			var oldnode = oldnodes[oldprocidx[node.name]];
 			node.x = oldnode.x;
 			node.y = oldnode.y;
 			node.z = oldnode.z;
 			node.rot = oldnode.rot;
 			node.tex = null;
-			if (node.proc.trap_exit == oldnode.proc.trap_exit && node.proc.registered_name == oldnode.proc.registered_name) {
+			if (node.type == 'proc' && node.proc.trap_exit == oldnode.proc.trap_exit && node.proc.registered_name == oldnode.proc.registered_name) {
 				node.tex = oldnode.tex;
 			}
 		} else {
@@ -114,9 +151,11 @@ function proc_loaded(result) {
 		}
 
 		node.v = [0,0,0];
-		node.color = defaultNodeColor;
-		if (node.proc.message_queue_len > 0) {
-			node.color = [1.0, 1.0, 0.3, 1.0];
+		if (node.type=='proc') {
+			node.color = defaultNodeColor;
+			if (node.proc.message_queue_len > 0) {
+				node.color = [1.0, 1.0, 0.3, 1.0];
+			}
 		}
 		if (node.tex == null) {
 			ctx.clearRect(0, 0, nodeTexSize, nodeTexSize);
@@ -124,28 +163,25 @@ function proc_loaded(result) {
 			ctx.textAlign = "center";
 			ctx.textBaseline = "middle";
 			ctx.fillStyle = "white";
-			circleText(ctx, node.proc.id, nodeTexSize/2, nodeTexSize/2, nodeTexSize*0.31, 0);
-			var name = "";
-			if (node.proc.registered_name && node.proc.registered_name.length>0) {
-				name = node.proc.registered_name;
-			} else {
-				name = node.proc.initial_call || "";
-			}
-			circleText(ctx, name, nodeTexSize/2, nodeTexSize/2, nodeTexSize*0.38, Math.PI * 0.3);
-			if (node.proc.trap_exit) {
-				ctx.font = "10px monospace";
-				circleText(ctx, "trap_exit", nodeTexSize/2, nodeTexSize/2, nodeTexSize*0.51, Math.PI * 0.2);
+			circleText(ctx, node.name, nodeTexSize/2, nodeTexSize/2, nodeTexSize*0.31, 0);
+			if (node.type=='proc') {
+				var procName = "";
+				if (node.proc.registered_name && node.proc.registered_name.length>0) {
+					procName = node.proc.registered_name;
+				} else {
+					procName = node.proc.initial_call || "";
+				}
+				circleText(ctx, procName, nodeTexSize/2, nodeTexSize/2, nodeTexSize*0.38, Math.PI * 0.3);
+				if (node.proc.trap_exit) {
+					ctx.font = "10px monospace";
+					circleText(ctx, "trap_exit", nodeTexSize/2, nodeTexSize/2, nodeTexSize*0.51, Math.PI * 0.2);
+				}
 			}
 			node.tex = GL.Texture.fromImage(canvas, {});
 		}
 	}
 
 	// console.timeEnd('proc_loaded');
-
-	for (var i=0; i<edges.length; i++) {
-		var edge = edges[i];
-		nodes[edge.from].edges.push(edge);
-	}
 
 	adjust_pos(10);
 	check_url_fragment();
@@ -155,19 +191,19 @@ function proc_loaded(result) {
 
 function adjust_pos(n) {
 	// position
-	var len = 1.0;
-	var maxv = 1;
+	var sk = 0.5;
 	for (var k=0; k<n; k++) {
+
 		for (var i=0; i<nodes.length; i++) {
 			var node = nodes[i];
 			var f = [0.0,0.0,0.0];
-			
+
 			var dd = node.x*node.x + node.y*node.y + node.z*node.z;
 			if (dd < 2.0) dd = 2.0
-			f[0] += -node.x * 0.1 / dd;
-			f[1] += -node.y * 0.1 / dd;
-			f[2] += -node.z * 0.1 / dd;
-	
+			f[0] += -node.x * 0.1 / dd * node.weight;
+			f[1] += -node.y * 0.1 / dd * node.weight;
+			f[2] += -node.z * 0.1 / dd * node.weight;
+
 			for (var j=0; j<nodes.length; j++) {
 				if (j == i) continue;
 				var node2 = nodes[j];
@@ -179,37 +215,40 @@ function adjust_pos(n) {
 				f[1] += dy * ff;
 				f[2] += dz * ff;
 			}
-			var sk = 0.3;
-			for (var j=0; j<node.links.length; j++) {
-				var idx = procidx[node.links[j]];
-				if (idx == null || idx == i) continue;
-				var node2 = nodes[idx];
-				var dx = node.x - node2.x, dy = node.y - node2.y, dz = node.z - node2.z;
-				var dd = dx*dx + dy*dy + dz*dz + 0.01;
-				var dn = Math.sqrt(dd);
 
-				var ll = len * Math.sqrt(node2.links.length + node.links.length - 1);
-				var ff = (dn-ll) * sk / dn;
-				f[0] -= dx * ff;
-				f[1] -= dy * ff;
-				f[2] -= dz * ff;
-			}
-			if (node.links.length > 1) {
-				// node weight = node.links.length
-				var r = 1.0 / (node.links.length-1);
-				f[0] *= r;
-				f[1] *= r;
-				f[2] *= r;
-			}
-			node.v[0] = node.v[0] * 0.7 + f[0];
-			node.v[1] = node.v[1] * 0.7 + f[1];
-			node.v[2] = node.v[2] * 0.7 + f[2];
+			node.v[0] += f[0] / node.weight2;
+			node.v[1] += f[1] / node.weight2;
+			node.v[2] += f[2] / node.weight2;
 		}
+
+		for (var i=0; i<edges.length; i++) {
+			var edge = edges[i];
+			if (edge.weight == 0) continue;
+			var node = nodes[edge.from];
+			var node2 = nodes[edge.to];
+
+			var dx = node.x - node2.x, dy = node.y - node2.y, dz = node.z - node2.z;
+			var dd = dx*dx + dy*dy + dz*dz;
+			var dn = Math.sqrt(dd) + 0.01;
+			var w = (dn-edge.len) * sk / dn;
+			var w1 = w / node.weight2;
+			var w2 = w / node2.weight2;
+			node.v[0] -= dx * w1;
+			node.v[1] -= dy * w1;
+			node.v[2] -= dz * w1;
+			node2.v[0] += dx * w2;
+			node2.v[1] += dy * w2;
+			node2.v[2] += dz * w2;
+		}
+
 		for (var i=0; i<nodes.length; i++) {
 			var node = nodes[i];
 			node.x += node.v[0];
 			node.y += node.v[1];
 			node.z += node.v[2];
+			node.v[0] *= 0.7;
+			node.v[1] *= 0.7;
+			node.v[2] *= 0.7;
 		}
 	}
 }
@@ -238,28 +277,30 @@ function init_gl() {
 	var tex01 = GL.Texture.fromURL('images/node_circle.png');
 	var tex03 = GL.Texture.fromURL('images/node_selected.png');
 	var tex02 = GL.Texture.fromURL('images/link_tex.png');
-	var linklines = new GL.Mesh({ coords: true, lines: true });
+	var edgelines = new GL.Mesh({ coords: true, lines: true });
 	var mesh = GL.Mesh.plane({coords: true});
 
 	gl.onupdate = function(seconds) {
 		rot += 45 * seconds;
-	
-		adjust_pos(1);
-	
-		linklines.vertices = [];
-		linklines.coords = [];
-		linklines.lines = [];
+
+		if (button == -1) {
+			adjust_pos(1);
+		}
+
+		edgelines.vertices = [];
+		edgelines.coords = [];
+		edgelines.lines = [];
 		for (var i=0; i<edges.length; i++) {
 			var edge = edges[i];
 			var from = nodes[edge.from];
 			var to = nodes[edge.to];
-			var d = edge.type == "link" ? 0 : 0.5;
-			linklines.vertices.push([from.x, from.y, from.z], [to.x, to.y, to.z]);
-			linklines.coords.push([0,d], [1,d]);
-			linklines.lines.push([i*2, i*2+1]);
+			var d = edge.line_uv;
+			edgelines.vertices.push([from.x, from.y, from.z], [to.x, to.y, to.z]);
+			edgelines.coords.push([0,d], [1,d]);
+			edgelines.lines.push([i*2, i*2+1]);
 		}
-		linklines.compile();
-	
+		edgelines.compile();
+
 		autoRefreshT += seconds;
 		if (autoRefresh && autoRefreshT > 30) {
 			refresh();
@@ -275,7 +316,7 @@ function init_gl() {
 		}
 		center = center.multiply(0.8).add(target_center.multiply(0.2));
 	};
-	
+
 	gl.ondraw = function() {
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 		gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE);
@@ -287,20 +328,20 @@ function init_gl() {
 		gl.rotate(angleX, 1, 0, 0);
 		gl.rotate(angleY, 0, 1, 0);
 		gl.translate(-center.x, -center.y, -center.z);
-	
+
 		for (var i=0; i<nodes.length; i++) {
 			var node = nodes[i];
 			gl.pushMatrix();
-	
+
 			gl.translate(node.x, node.y, node.z);
-	
+
 			// billboard
 			gl.rotate(-angleY, 0, 1, 0);
 			gl.rotate(-angleX, 1, 0, 0);
-	
+
 			tex01.bind(0);
 			gl.rotate(rot  + node.rot, 0, 0, 1);
-	
+
 			if (node == selected) {
 				tex03.bind(1);
 				shader.uniforms({
@@ -308,41 +349,52 @@ function init_gl() {
 					color: [0,1,0,1]
 				}).draw(mesh);
 			}
-	
+
 			shader.uniforms({
 				texture: 0,
 				color: node.color
 			}).draw(mesh);
-	
-			gl.pushMatrix();
-			gl.scale(0.94, 0.94, 0.94);
-			gl.rotate(-rot*1.6 + node.rot * 3, 0, 0, 1);
-			shader.draw(mesh);
-			gl.popMatrix();
-	
+
+			if (node.type == 'proc') {
+				gl.pushMatrix();
+				gl.scale(0.94, 0.94, 0.94);
+				gl.rotate(-rot*1.6 + node.rot * 3, 0, 0, 1);
+				shader.draw(mesh);
+				gl.popMatrix();
+			} else if (node.type == 'node') {
+				gl.pushMatrix();
+				gl.scale(2,2,2);
+				gl.rotate(-rot*1.6 + node.rot * 3, 0, 0, 1);
+				shader.draw(mesh);
+				gl.popMatrix();
+			}
+
 			node.tex.bind(0);
 			shader.draw(mesh);
-	
+
 			gl.popMatrix();
 		}
-		
-		if (linklines.lines.length > 0) {
+
+		if (edgelines.lines.length > 0) {
 			tex02.bind(0);
 			gl.lineWidth(2);
 			shader.uniforms({
 				texture: 0,
 				color: [1.0, 1.0, 1.0, 1.0]
-			}).draw(linklines,gl.LINES);
+			}).draw(edgelines,gl.LINES);
 		}
-	
+
 	};
 
 	gl.onmousedown = function(e) {
 		e.preventDefault();
 		button = e.button;
 		drag = false;
-	}
-	
+	};
+	gl.onmouseup = function(e) {
+		button = -1;
+	};
+
 	gl.onmousemove = function(e) {
 		if (e.dragging) {
 			if (e.deltaX*e.deltaX + e.deltaY * e.deltaY > 2) {
@@ -365,7 +417,7 @@ function init_gl() {
 
 
 // event
-var button = 0;
+var button = -1;
 var drag = false;
 var dragX = 0, dragY = 0;
 
@@ -441,7 +493,7 @@ window.addEventListener('load',(function(e){
 	gl.canvas.addEventListener('dblclick', function(e){
 		e.preventDefault();
 		var target = node_by_pos(e.x, e.y);
-		if (target && target == selected) {
+		if (target && target == selected && target.type == 'proc') {
 			console.log("dblclick: " + target.name);
 			$('#proc_info_id').innerText=target.name;
 			element_clear($('#proc_info_list'));
@@ -458,6 +510,12 @@ window.addEventListener('load',(function(e){
 						requestJson("DELETE", "nodes/" + m[1] + "/procs/" + m[2], refresh).send();
 					}
 				}).show();
+		}
+		if (target && target == selected && target.type == 'remote_proc') {
+			location.href = "?node=" + target.node;
+		}
+		if (target && target == selected && target.type == 'node') {
+			location.href = "?node=" + target.node;
 		}
 	});
 
@@ -497,6 +555,18 @@ window.addEventListener('load',(function(e){
 		if (e.keyCode == 'Y'.charCodeAt(0)) {
 			autoRefresh = !autoRefresh;;
 			new Toast(null, "Polling: " + autoRefresh).show();
+		}
+		if (e.keyCode == 'W'.charCodeAt(0)) {
+			cameraPos.z = Math.min(cameraPos.z + 0.5, -0.1);
+		}
+		if (e.keyCode == 'S'.charCodeAt(0)) {
+			cameraPos.z = Math.max(cameraPos.z - 0.5, -150);
+		}
+		if (e.keyCode == 'A'.charCodeAt(0)) {
+			angleY += 1.0;
+		}
+		if (e.keyCode == 'D'.charCodeAt(0)) {
+			angleY -= 1.0;
 		}
 	});
 
